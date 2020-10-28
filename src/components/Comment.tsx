ad61ebb70bdd6dca5edc89dc0eb7d09da3080c99
Alex Sapoznikov <alex@nodeswat.com>
2020-10-28 09:14:24 +0200

    Use replaceWith instead replaceChild

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -11,6 +11,7 @@ class ReactComment extends React.PureComponent<IReactCommentProps> {
     onLoaded: noop,
   };
 
+  // @ts-ignore
   private parentNode: Node;
 
   private currentNode: Element;
@@ -26,7 +27,7 @@ class ReactComment extends React.PureComponent<IReactCommentProps> {
     this.currentNode = node;
     this.parentNode = node.parentNode as Node;
     try {
-      this.parentNode.replaceChild(commentNode, node);
+      node.replaceWith(commentNode);
     } catch (err) {
       console.error('Try-Catched KeepAlive error', err)
     }
@@ -36,7 +37,7 @@ class ReactComment extends React.PureComponent<IReactCommentProps> {
 
   public componentWillUnmount() {
     try {
-      this.parentNode.replaceChild(this.currentNode, this.commentNode);
+      this.commentNode.replaceWith(this.currentNode);
     } catch (err) {
       console.error('Try-Catched KeepAlive error', err)
     }
